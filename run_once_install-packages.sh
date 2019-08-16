36d5efc7e4f063d51894b1cf92f6f73c5105b045
Jarrod Pas <jarrod@jarrodpas.com>
2019-08-16 17:34:43 -0600

    Adds package installer

---
+++ run_once_install-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nix-env -iA \
+  nixpkgs.bat \
+  nixpkgs.exa \
+  nixpkgs.fd \
+  nixpkgs.fzf \
+  nixpkgs.htop \
+  nixpkgs.neovim \
+  nixpkgs.ripgrep \
+  nixpkgs.tmux \
+  nixpkgs.chezmoi
