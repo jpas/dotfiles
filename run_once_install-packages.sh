4f8a4a7f372e7e4b71a7a559131165e6e97777d4
Jarrod Pas <jarrod@jarrodpas.com>
2019-08-17 13:38:30 -0600

    Adds bash 5.0 to nix-env

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -4,6 +4,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nix-env -iA \
+  nixpkgs.bashInteractive_5 \
   nixpkgs.bat \
   nixpkgs.exa \
   nixpkgs.fd \
