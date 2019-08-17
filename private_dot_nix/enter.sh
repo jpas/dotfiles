591cd4ca4d1abd257459071fab005ab038708a52
Jarrod Pas <jarrod@jarrodpas.com>
2019-08-17 00:12:30 -0600

    More updates

--- private_dot_nix/enter.sh
+++ private_dot_nix/enter.sh
@@ -1,12 +1,12 @@
-# fix ldap name resolution
-export LD_PRELOAD="/lib64/libnss_ldap.so.2"
 
 nix=$HOME/.nix
 chroot="$nix/nix-user-chroot-$(uname -m)"
 
-# if we are not in the chroot this will be a broken symlink
-if ! [[ -d "$HOME/.nix-profile" ]]; then
-  if [[ -x "$chroot" ]]; then
+if [[ -x "$chroot" ]]; then
+  # fix ldap name resolution
+  export LD_PRELOAD="/lib64/libnss_ldap.so.2"
+  # if we are not in the chroot this will be a broken symlink
+  if ! [[ -d "$HOME/.nix-profile" ]]; then
     exec "$chroot" "$nix" "$SHELL"
   fi
 fi
