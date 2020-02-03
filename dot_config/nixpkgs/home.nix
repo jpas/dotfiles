19c9fbe0ee078c8995c969e0fd05d942651e05e4
Jarrod Pas <jarrod@jarrodpas.com>
2020-02-03 01:47:00 -0600

    feat: add alacritty to i3

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Let Home Manager install and manage itself.
@@ -19,7 +19,64 @@
     tmux
   ];
 
+  xsession.enable = true;
+  xsession.windowManager.i3 = {
+    enable = true;
+    config = let
+      modifier = "Mod4"; # meta key
+    in {
+      modifier = modifier;
+      fonts = [ "Jetbrains Mono 10" ];
+      keybindings = lib.mkOptionDefault {
+        "${modifier}+Return" = "exec alacritty";
+      };
+    };
+  };
+
   # TODO: enable programs from packages
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font = {
+        normal.family = "JetBrains Mono";
+        size = 10;
+      };
+      colors = {
+        # Gruvbox dark
+        primary = {
+          # hard contrast: background = "0x1d2021"
+          background = "0x282828";
+          # soft contrast: background = "0x32302f"
+          foreground = "0xebdbb2";
+        };
+
+        # Normal colors
+        normal = {
+          black   = "0x282828";
+          red     = "0xcc241d";
+          green   = "0x98971a";
+          yellow  = "0xd79921";
+          blue    = "0x458588";
+          magenta = "0xb16286";
+          cyan    = "0x689d6a";
+          white   = "0xa89984";
+        };
+
+        # Bright colors
+        bright = {
+          black   = "0x928374";
+          red     = "0xfb4934";
+          green   = "0xb8bb26";
+          yellow  = "0xfabd2f";
+          blue    = "0x83a598";
+          magenta = "0xd3869b";
+          cyan    = "0x8ec07c";
+          white   = "0xebdbb2";
+        };
+      };
+    };
+  };
 
   # emacs? emacs!
   programs.emacs.enable = true;
