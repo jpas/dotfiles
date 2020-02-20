27419e522058622098dbbb33235d40373c0dd31b
Jarrod Pas <j.pas@usask.ca>
2020-02-20 14:50:59 -0600

    fix: bash is inconsistend on macOS

--- dot_config/bash/profile.d/00-base.sh
+++ dot_config/bash/profile.d/00-base.sh
@@ -1,23 +1,38 @@
 # set up our permissions right...
 umask 027
 
+# setting up PATH because no-one seems to do it the same way...
+PATH=''
+for d in /{usr/{local/,},}{,s}bin; do
+  if [ -d "$d" ]; then
+    PATH="${PATH}${PATH:+:}${d}"
+  fi
+done
+export PATH
+
 if [ -d "$HOME/.nix-profile/etc/profile.d" ]; then
-  for f in $HOME/nix-profile/etc/profile.d/*.sh; do
+  for f in $HOME/.nix-profile/etc/profile.d/*.sh; do
     if [ -r $f ]; then
       source $f
     fi
   done
 fi
 
-export PATH=$HOME/.local/bin:$PATH
+# make sure that my bin is first
+export PATH=$HOME/.local/bin${PATH:+:}$PATH
 
+# cd shortcuts
 export CDPATH=$HOME/school/classes
 
 # no history
 export HISTFILE=/dev/null
 export LESSHISTFILE=/dev/null
 
-# set up out editors
+rm -f $HOME/.bash_history
+rm -f $HOME/.lesshist
+rm -f $HOME/.python_history
+
+# set up our editors
 export VISUAL=vim
 export EDITOR=vim
 export PAGER=less
